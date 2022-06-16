9183232f87e29c9e2eaf0b23185b1afe653b21c0
Charles Roth <charlieroth4@icloud.com>
2022-06-16 20:29:50 +0200

    todo: add timer model
    [timer example](https://github.com/charmbracelet/bubbletea/blob/master/examples/timer/main.go)

--- main.go
+++ main.go
@@ -15,27 +15,26 @@ const (
 	Working               = "Working"
 )
 
-// working duration: 15, 20, 25, 30, 45, 50, 60, 90
-// break duration: 5, 10, 15, 20
-// session count: 4, 5, 6, 7
-
 type WorkingDuration struct {
-	choices  []string // options in working duration
-	cursor   int      // which working duration option cursor is point at
-	selected string   // which working duration was selected
+	choices  []string
+	cursor   int
+	selected string
 }
 
 type BreakDuration struct {
-	choices  []string // options in working duration
-	cursor   int      // which working duration option cursor is point at
-	selected string   // which working duration was selected
+	choices  []string
+	cursor   int
+	selected string
 }
 
 type SessionCount struct {
-	choices  []string // options in working duration
-	cursor   int      // which working duration option cursor is point at
-	selected string   // which working duration was selected
+	choices  []string
+	cursor   int
+	selected string
 }
+
+// TODO(charlieroth): Add timer model
+// https://github.com/charmbracelet/bubbletea/blob/master/examples/timer/main.go
 
 type State struct {
 	Step            string
@@ -57,42 +56,42 @@ func (s State) HasSelectedSessionCount() bool {
 }
 
 func (s State) CurrentCursor() int {
-    switch s.Step {
-    case ChooseWorkingDuration:
-        return s.WorkingDuration.cursor
-    case ChooseBreakDuration:
-        return s.BreakDuration.cursor
-    case ChooseSessionCount:
-        return s.SessionCount.cursor
-    }
+	switch s.Step {
+	case ChooseWorkingDuration:
+		return s.WorkingDuration.cursor
+	case ChooseBreakDuration:
+		return s.BreakDuration.cursor
+	case ChooseSessionCount:
+		return s.SessionCount.cursor
+	}
 
-    return 0
+	return 0
 }
 
 func (s State) CurrentSelectedChoice() string {
-    switch s.Step {
-    case ChooseWorkingDuration:
-        return s.WorkingDuration.selected
-    case ChooseBreakDuration:
-        return s.BreakDuration.selected
-    case ChooseSessionCount:
-        return s.SessionCount.selected
-    }
+	switch s.Step {
+	case ChooseWorkingDuration:
+		return s.WorkingDuration.selected
+	case ChooseBreakDuration:
+		return s.BreakDuration.selected
+	case ChooseSessionCount:
+		return s.SessionCount.selected
+	}
 
-    return ""
+	return ""
 }
 
 func (s State) CurrentChoices() []string {
-    switch s.Step {
-    case ChooseWorkingDuration:
-        return s.WorkingDuration.choices
-    case ChooseBreakDuration:
-        return s.BreakDuration.choices
-    case ChooseSessionCount:
-        return s.SessionCount.choices
-    }
+	switch s.Step {
+	case ChooseWorkingDuration:
+		return s.WorkingDuration.choices
+	case ChooseBreakDuration:
+		return s.BreakDuration.choices
+	case ChooseSessionCount:
+		return s.SessionCount.choices
+	}
 
-    return []string{}
+	return []string{}
 }
 
 func InitState() State {
@@ -125,44 +124,44 @@ func HandleQuit(s State) (tea.Model, tea.Cmd) {
 }
 
 func HandleUp(s State) (tea.Model, tea.Cmd) {
-    switch s.Step {
+	switch s.Step {
 	case ChooseWorkingDuration:
-        if s.WorkingDuration.cursor > 0 {
-            s.WorkingDuration.cursor--
-        }
-        return s, nil
+		if s.WorkingDuration.cursor > 0 {
+			s.WorkingDuration.cursor--
+		}
+		return s, nil
 	case ChooseBreakDuration:
-        if s.BreakDuration.cursor > 0 {
-            s.BreakDuration.cursor--
-        }
-        return s, nil
+		if s.BreakDuration.cursor > 0 {
+			s.BreakDuration.cursor--
+		}
+		return s, nil
 	case ChooseSessionCount:
-        if s.SessionCount.cursor > 0 {
-            s.SessionCount.cursor--
-        }
-        return s, nil
+		if s.SessionCount.cursor > 0 {
+			s.SessionCount.cursor--
+		}
+		return s, nil
 	}
 
 	return s, nil
 }
 
 func HandleDown(s State) (tea.Model, tea.Cmd) {
-    switch s.Step {
+	switch s.Step {
 	case ChooseWorkingDuration:
-        if s.WorkingDuration.cursor < len(s.WorkingDuration.choices)-1 {
-            s.WorkingDuration.cursor++
-        }
-        return s, nil
+		if s.WorkingDuration.cursor < len(s.WorkingDuration.choices)-1 {
+			s.WorkingDuration.cursor++
+		}
+		return s, nil
 	case ChooseBreakDuration:
-        if s.BreakDuration.cursor < len(s.BreakDuration.choices)-1 {
-            s.BreakDuration.cursor++
-        }
-        return s, nil
+		if s.BreakDuration.cursor < len(s.BreakDuration.choices)-1 {
+			s.BreakDuration.cursor++
+		}
+		return s, nil
 	case ChooseSessionCount:
-        if s.SessionCount.cursor < len(s.SessionCount.choices)-1 {
-            s.SessionCount.cursor++
-        }
-        return s, nil
+		if s.SessionCount.cursor < len(s.SessionCount.choices)-1 {
+			s.SessionCount.cursor++
+		}
+		return s, nil
 	}
 
 	return s, nil
@@ -193,40 +192,40 @@ func HandleConfirm(s State) (tea.Model, tea.Cmd) {
 }
 
 func HandleEnter(s State) (tea.Model, tea.Cmd) {
-    switch s.Step {
+	switch s.Step {
 	case ChooseWorkingDuration:
-        if s.WorkingDuration.selected == "" {
-            s.WorkingDuration.selected = s.WorkingDuration.choices[s.WorkingDuration.cursor]
-        } else {
-            if s.WorkingDuration.choices[s.WorkingDuration.cursor] != s.WorkingDuration.selected {
-                s.WorkingDuration.selected = s.WorkingDuration.choices[s.WorkingDuration.cursor]
-            } else {
-                s.WorkingDuration.selected = ""
-            }
-        }
-        return s, nil
+		if s.WorkingDuration.selected == "" {
+			s.WorkingDuration.selected = s.WorkingDuration.choices[s.WorkingDuration.cursor]
+		} else {
+			if s.WorkingDuration.choices[s.WorkingDuration.cursor] != s.WorkingDuration.selected {
+				s.WorkingDuration.selected = s.WorkingDuration.choices[s.WorkingDuration.cursor]
+			} else {
+				s.WorkingDuration.selected = ""
+			}
+		}
+		return s, nil
 	case ChooseBreakDuration:
-        if s.BreakDuration.selected == "" {
-            s.BreakDuration.selected = s.BreakDuration.choices[s.BreakDuration.cursor]
-        } else {
-            if s.BreakDuration.choices[s.BreakDuration.cursor] != s.BreakDuration.selected {
-                s.BreakDuration.selected = s.BreakDuration.choices[s.BreakDuration.cursor]
-            } else {
-                s.BreakDuration.selected = ""
-            }
-        }
-        return s, nil
+		if s.BreakDuration.selected == "" {
+			s.BreakDuration.selected = s.BreakDuration.choices[s.BreakDuration.cursor]
+		} else {
+			if s.BreakDuration.choices[s.BreakDuration.cursor] != s.BreakDuration.selected {
+				s.BreakDuration.selected = s.BreakDuration.choices[s.BreakDuration.cursor]
+			} else {
+				s.BreakDuration.selected = ""
+			}
+		}
+		return s, nil
 	case ChooseSessionCount:
-        if s.SessionCount.selected == "" {
-            s.SessionCount.selected = s.SessionCount.choices[s.SessionCount.cursor]
-        } else {
-            if s.SessionCount.choices[s.SessionCount.cursor] != s.SessionCount.selected {
-                s.SessionCount.selected = s.SessionCount.choices[s.SessionCount.cursor]
-            } else {
-                s.SessionCount.selected = ""
-            }
-        }
-        return s, nil
+		if s.SessionCount.selected == "" {
+			s.SessionCount.selected = s.SessionCount.choices[s.SessionCount.cursor]
+		} else {
+			if s.SessionCount.choices[s.SessionCount.cursor] != s.SessionCount.selected {
+				s.SessionCount.selected = s.SessionCount.choices[s.SessionCount.cursor]
+			} else {
+				s.SessionCount.selected = ""
+			}
+		}
+		return s, nil
 	}
 
 	return s, nil
@@ -287,22 +286,22 @@ func GetPrompt(s State) string {
 func RenderChoice(s State, cursor, checked, choice string) string {
 	switch s.Step {
 	case ChooseWorkingDuration:
-        return fmt.Sprintf("%s [%s] %s mins\n", cursor, checked, choice)
+		return fmt.Sprintf("%s [%s] %s mins\n", cursor, checked, choice)
 	case ChooseBreakDuration:
-        return fmt.Sprintf("%s [%s] %s mins\n", cursor, checked, choice)
+		return fmt.Sprintf("%s [%s] %s mins\n", cursor, checked, choice)
 	case ChooseSessionCount:
-        return fmt.Sprintf("%s [%s] %s sessions\n", cursor, checked, choice)
+		return fmt.Sprintf("%s [%s] %s sessions\n", cursor, checked, choice)
 	}
 
-    return ""
+	return ""
 }
 
 func ChoicesView(s State) string {
 	view := ""
 
-    currentCursor := s.CurrentCursor()
-    currentSelectedChoice := s.CurrentSelectedChoice()
-    currentChoices := s.CurrentChoices()
+	currentCursor := s.CurrentCursor()
+	currentSelectedChoice := s.CurrentSelectedChoice()
+	currentChoices := s.CurrentChoices()
 
 	for i, choice := range currentChoices {
 		cursor := " "
